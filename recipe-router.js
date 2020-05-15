6122e3553b9b9e2f2c2c67c27bb40828d71e4d27
andrewhpark25 <andrewhpark25@gmail.com>
2020-05-15 07:56:19 -0700

    push

--- recipe-router.js
+++ recipe-router.js
@@ -31,6 +31,22 @@ router.get('/:id/ingredients', (req, res) => {
   });
 });
 
+router.get('/:id/instructions', (req, res) => {
+    const { id } = req.params;
+  
+    Recipes.getInstructions(id)
+    .then(instructions => {
+      if (instructions.length) {
+        res.json(instructions);
+      } else {
+        res.status(404).json({ message: 'Could not find instructions for given recipe' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get instructions' });
+    });
+  });
+  
 
 
 module.exports = router;
